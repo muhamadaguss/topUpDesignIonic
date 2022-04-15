a642e3e5b6bf5f952fc56399a05d9b93bf98947c
BabyKing <muhamadagus3197@gmail.com>
2022-04-16 05:18:48 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  features: any[] = [
+    {id:1,name:"Top Up",src:"assets/icons/top-up.png",background:'background: rgba(45, 211, 111, 0.1)',page:''},
+    {id:2,name:"Withdraw",src:"assets/icons/cash-withdrawal.png",background:'background: rgba(45, 211, 111, 0.1)',page:''},
+    {id:3,name:"Send",src:"assets/icons/send.png",background:'background: rgba(45, 211, 111, 0.1)',page:''},
+    {id:4,name:"Pay",src:"assets/icons/debit-card.png",background:'background: rgba(45, 211, 111, 0.1)',page:''},
+  ];
+
+  transactions: any[] = [
+    {id:1,vendor:"Flaticons",image:"",amount:"100000",time:'03:00 PM',},
+    {id:2,vendor:"Phone Transfer",image:"",amount:"-100000",time:'04:00 PM',}
+  ];
+
   constructor() {}
 
 }
